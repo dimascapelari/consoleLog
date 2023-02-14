4c9ffad2c1e57b2691bb810e8d16f81fcad72dfd
Dimas Capelari <97951537+dimascapelari@users.noreply.github.com>
2023-02-14 09:06:31 -0300

    Estudos no console.log

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+/*
 console.log(0.1 + 0.2 === 0.3);
-console.log(0.1 + 0.2 === 0.3);
-console.log(0.1 + 0.2 === 0.3);
-console.log(0.1 + 0.2 === 0.3);
-// false
+//Resposta: false
+*/
+
+const arr = [1, 2, 3, [4, 5, 6, [7, 5, 4]]];
+console.log(arr.flat(0));
+//Resposta: [ 1, 2, 3, [ 4, 5, 6, [ 7, 5, 4 ] ] ]
