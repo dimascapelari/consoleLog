0f1af425254723323d7ed73412ec3cd607355697
Dimas Capelari <97951537+dimascapelari@users.noreply.github.com>
2023-04-06 08:54:02 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+const promise1 = new Promise((resolve, reject) => {
+  setTimeout(() => resolve("Hello"), 2000);
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => resolve("World"), 1000);
+});
+Promise.all([promise1, promise2]).then((values) => console.log(values));
+//Resposta: [ 'Hello', 'World' ]
+
 //---------------------------------------------
 function parameter(para = 10) {
   console.log(para);
