ef67db618cec40eb2ed08ee6ffff3807d9f11863
Dimas Capelari <97951537+dimascapelari@users.noreply.github.com>
2023-07-20 16:46:11 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
+//---------------------------------------------
 const numbers = [1, 2, 3, 4, 5];
 const squaredNumbers = numbers.map(num => num ** 2);
 console.log(squaredNumbers);
 //Resosta: [1,4,9,16,25]
 
 //---------------------------------------------
-
 Symbol("a") == Symbol("a")
+
 //---------------------------------------------
 
 let a = 0;
