49bf9d1014fc7b7c27c2f30a82f579bc1f41947c
Dimas Capelari <97951537+dimascapelari@users.noreply.github.com>
2023-07-19 23:07:03 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const numbers = [1, 2, 3, 4, 5];
+const squaredNumbers = numbers.map(num => num ** 2);
+console.log(squaredNumbers);
+//---------------------------------------------
 
 Symbol("a") == Symbol("a")
 //---------------------------------------------
